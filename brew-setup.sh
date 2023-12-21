44759ab15177b190c5fbc72d72cc9cafaaf53988
Avhi Mazumder <avhimazumder5@outlook.com>
2023-12-21 19:09:40 +0530

    Added script file

---
+++ brew-setup.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+
+function is_arch() {
+	if [ -f "/etc/arch-release" ]; then
+		return 0
+	else
+		return 1
+	fi
+}
+
+function is_debian() {
+	if [ -f "/etc/debian-release"]; then
+		return 0
+	else
+		return 1
+	fi
+}
+
+if is_arch; then
+	echo "Arch-based distribution is detected. Installing Homebrew for Arch... "
+	sudo pacman -Syyu --noconfirm
+	sudo pacman -S base-devel --noconfirm
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+	echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >>~/.zshrc
+	eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+	echo "Homebrew installation completed. Please restart your terminal or run 'source ~/.zshrc' to start using Homebrew."
+
+elif is_debian; then
+	echo "Debian-based distribution is detected. Installing Homebrew for Debian..."
+	sudo apt-get update && upgrade
+	sudo apt-get install -y build-essential curl file git
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >>~/.bashrc
+	eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+	echo "Homebrew installation completed for Debian. Please restart your terminal or run 'source ~/.bashrc' to start using Homebrew."
+
+else
+	echo "Unsupported Distribution"
+	exit 1
+
+fi
